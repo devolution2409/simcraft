ec5ccabb556021dc4f1ee04bbcb985770e7a34a7
Devolution <contact@davidbouet.fr>
2019-02-15 21:57:13 +0100

    SSE seems to work

--- index.js
+++ index.js
@@ -2,8 +2,8 @@
 let express = require('express');
 let fs = require('fs');
 let app = express();
-const exec = require('await-exec');
-
+//const exec = require('await-exec');
+const { spawn } = require('child_process');
 
 
 // checking if the variables were set, if they weren't, we serve a page saying they weren't.. DUH
@@ -52,7 +52,12 @@ app.get("/eu/medivh/nevess",function(req,res,next){
 });
 
 app.get(/\/(EU|NA|KR|TW)\/\w+\/\w+/i,function(req,res,next){
-	//res.send("Pog");
+	//needed to register this as SSE
+	res.status(200).set({
+		"connection": "keep-alive",
+		"cache-control": "no-cache",
+		"content-Type": "text/event-stream"
+	});
 	let path = req.originalUrl;
 	// path = /eu/suramar/devolution (with or without trailing /)
 	path = path.substring(1);
@@ -66,10 +71,52 @@ let file = uuid + '.html';
 let json = uuid + '.json';
 // variable function here because we need async to be able to await the response before sending it
 var lul = async function(){
-	// calculate_scale_factors=1 requires at least >100000 iterations
-	await exec( "../engine/simc target_error=0.05" + " armory=" + infos[0] + "," + infos[1] + "," + infos [2] + " html=" + file + " json=" + json)
-	// search how to output stout again forsenE (even tho await-exec might fuck it up)
-	.then( (data) => {
+	// calculate_scale_factors=1 requires at least >10000 iterations
+	let armoryString =  "armory=" + infos[0] + "," + infos[1] + "," + infos[2]
+	const child = spawn ( "../engine/simc", [armoryString,"target_error=0.05","html=" + file,"json=" + json,"calculate_scale_factors=1","iterations=10000","fight_style=patchwerk"]);
+	//fetching stdout
+	let currentStep = 0;
+	let stderr = [];
+	for await (const err of child.stderr){
+		stderr.push(`${err}`);
+	}
+
+
+	for await (const data of child.stdout) {
+		if (data.includes("Simulating...")){
+			console.log(`${data}`);
+		}
+  	//	console.log(`stdout from the child: ${data}`);
+	// need to apply regexp to stdout to find out which step we at
+		let regexp = RegExp(/(Generating \w+): (\d+)\/(\d+) \W+ \d+\/\d+ \w+=\d+ \w+=\d+(\.)?\d+% (\d+min)? ?(\d+sec)?/gm);
+		let matches;
+		// if we have a match we can write it down
+		matches = regexp.exec(data);
+		if ( matches !== null){
+			//if current step has changed, send partial data to client
+			if (currentStep !== matches[2]){
+				currentStep = matches[2];
+				
+				let temp ={
+						"maximum_steps": matches[3],
+						"current_step" : matches[2],
+						"step_name" : matches[1],
+						"estimated_time" : matches[5] + matches[6]
+						};
+				// \n\n is important to denote data packed it ended
+				console.log("Sending partial data to client:" + JSON.stringify(temp));	
+				res.write("data:" + JSON.stringify(temp) + "\n\n");
+			}	
+		}
+			//console.log("step: " + matches[1] + " " +  matches[2] + "/" + matches[3] + "estimated time:" + matches[5] + matches[6]);
+	}
+	
+
+	//adding handler on exit :)
+
+	child.on('exit', (code) => {
+	console.log("Exited simcraft with code: " + code);
+	if (code === 0){
 	console.log("Generated: " + file );
 	console.log("Generated: " + json );
 	// move the file to the right folder because renderer will search in /views/
@@ -94,39 +141,38 @@ var lul = async function(){
 	fs.readFile("/simc/web/views/" + json, (err,d)=>{
                         if (err) res.send(err);
                         let obj = JSON.parse(d);
-		//	if (obj.hasOwnProperty('git_revision')){
-		//		res.send("Git revision:" + obj['git_revision']);
-		//	}
-                        res.json(obj);
+			console.log("Sending the final packet containing the simc json");
+			res.write("data:" + JSON.stringify(obj) + "\n\n");
+			res.end();
         });
 	
+	//TODO: upload json to databse with charname and last sim date, and git revision
+	// if (charAlreadySimmed not a long time ago)
+	//       serve the json
+	// if charalreadysimmed but too long ago
+	//       sim again, upload to db
+        // if char IS BEING simmed, wait for the other simulation to run and serve results
+
 	// remove the html file 
 	fs.unlink('/simc/web/views/' + uuid + '.html', (err) => { 
 			if (err) throw err;
 			else console.log('File removed from /views/');
 			});
+	
+
 	console.log("Simulation request successfully processed!");
 		// if exception comes from the command, it will have stderr
 		// if stderr contains Char not found, we redirect it to the main page forsenE
 		//TODO: api call myself with ajax to see if char exist :)
-	})
-	.catch( (err) => {
-		if(err.hasOwnProperty('stderr'))
-			res.json({'error' : err.stderr});
-			/*		
-			if (err.stderr.includes("Realm not found")){
-				console.log(`Realm: ${infos[1]} not found !`);
-				res.send("Realm not found!");
-			}else if (err.stderr.includes("Character not found")){
-				res.send("Character not found!");
-				console.log(`Realm: ${infos[2]} not found !`);
 
-			}
-			console.log(err);*/
-
-	});
-
-
+	}else{
+		//let temp = "data: exited simc with status code: " + code + "\n\n";
+		let temp = "data: " + stderr[0] + "\n\n";
+		res.send(temp);
+		res.end();
+		console.log(stderr[0]);
+	}
+	}); // end on exit
 };
 	lul();
 });
@@ -156,15 +202,8 @@ app.listen(80);
 // curl http://ifconfig.me/ip
 fs.access('/.dockerenv', fs.F_OK, (err) => {
 		if (!err){
-			let host = '';
-			let getIp = async () => {
-			exec("curl http://ifconfig.me/ip").then( (data) =>{
-				host = data.stdout;
 				console.log("Application listening to port 80!\n\nOops! It appears you are running this node application through docker. The application will be available at host:externalport");
 
-				});
-			};
-			getIp();
 		}else{
 			console.log("Application ready to listen on port 80");
 		}
